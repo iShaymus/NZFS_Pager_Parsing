bb2669f6ad9bb21e77330f557c725c2bda8e2c04
iShaymus <iShaymus@users.noreply.github.com>
2015-06-01 18:53:41 +1200

    Built and tested class PagerMessage

--- NZFSPP.py
+++ NZFSPP.py
@@ -5,43 +5,61 @@ import sys
 # (UNIT123, UNIT1234) 111-STRU (Alarm Type SMOKE) (Box 123) SOME BUILDING SOME ADDRESS. (XStr A STREET) .JOB DETAILS. #F12345
 # the fields (Alarm Type *) and (Box *) are optional and do not appear in every message.
 
-sInput = str(sys.argv[1:])
 
-# Matches appliance names using the consistent four uppercase letters followed by three - four numbers.
-pRespondingUnits = ','.join(re.findall(r'\w[A-Z]{3}\d[0-9]{2,3}', sInput))
+class PagerMessage:
 
-# Matches source and job type using the - as a guide, this section is always proceeded by the trucks on the job
-# therefore is always proceeded by a ) and a space. Allows between 3-9 characters either side of the - this is
-# to allow such variations as 111-RESC, CAA-AIRCRAFT etc.
-pJobSource = ''.join(re.findall(r'\) ([A-Za-z1-9]{2,8}-[A-Za-z1-9]{2,8})', sInput))
+    def __init__(self, message):
+        self.message = message
 
-# Gets address by starting at (but ignoring) the job source e.g. -RESC and capturing everything until the next . period
-# the end of the address section always has a period. Uses ?; to ignore up to two sets of brackets that may appear in
-# the string for things such as box numbers or alarm types.
-pAddress = ''.join(re.findall(r'-[A-Z1-9]{2,8}(?: \(Alarm .*?\))?(?: \(Box .*\))? (.*?)\. \(', sInput))
+    def appliances(self):
+        # Matches appliance names using the consistent four uppercase letters followed by three - four numbers.
+        appliances = ','.join(re.findall(r'\w[A-Z]{3}\d[0-9]{2,3}', self.message))
+        return appliances
 
-# Finds the specified cross streets as they are always within () brackets, each bracket has a space immediately
-# before or after and the work XStr is always present.
-pCrossStreet = ''.join(re.findall(r' \((XStr.*?)\) ', sInput))
+    def source(self):
+        # Matches source and job type using the - as a guide, this section is always proceeded by the trucks on the job
+        # therefore is always proceeded by a ) and a space. Allows between 3-9 characters either side of the - this is
+        # to allow such variations as 111-RESC, CAA-AIRCRAFT etc.
+        source = ''.join(re.findall(r'\) ([A-Za-z1-9]{2,8}-[A-Za-z1-9]{2,8})', self.message))
+        return source
 
-# The job details / description is always contained between two . periods e.g.  .42YOM CARDIAC ARREST.  each period
-# has a space either immediately before or after.
-pJobDetails = ''.join(re.findall(r' \.(.*?)\. ', sInput))
+    def address(self):
+        # Gets address by starting at (but ignoring) the job source e.g. -RESC and capturing everything until the next . period
+        # the end of the address section always has a period. Uses ?; to ignore up to two sets of brackets that may appear in
+        # the string for things such as box numbers or alarm types.
+        address = ''.join(re.findall(r'-[A-Z1-9]{2,8}(?: \(Alarm .*?\))?(?: \(Box .*\))? (.*?)\. \(', self.message))
+        return address
 
-# Job number is always in the format #F followed by seven digits.  The # is always proceeded by a space.  Allowed
-# between 1 and 8 digits for future proofing.
-pJobNumber = ''.join(re.findall(r' (#F\d{0,7})', sInput))
+    def xstr(self):
+        # Finds the specified cross streets as they are always within () brackets, each bracket has a space immediately
+        # before or after and the work XStr is always present.
+        xstr = ''.join(re.findall(r' \((XStr.*?)\) ', self.message))
+        return xstr
 
-# Get optional Alarm type which is always presented with a space (Alarm
-pAlarmDetails = ''.join(re.findall(r' \((Alarm .*?)\) ', sInput))
+    def details(self):
+        # The job details / description is always contained between two . periods e.g.  .42YOM CARDIAC ARREST.  each period
+        # has a space either immediately before or after.
+        job_details = ''.join(re.findall(r' \.(.*?)\. ', self.message))
+        return job_details
 
-# Get optional Box type which is always presented with a space (Box
-pBoxDetails = ''.join(re.findall(r' (\(Box .*?\))', sInput))
+    def job_number(self):
+        # Job number is always in the format #F followed by seven digits.  The # is always proceeded by a space.  Allowed
+        # between 1 and 8 digits for future proofing.
+        job_number = ''.join(re.findall(r' (#F\d{0,7})', sInput))
+        return job_number
 
-print "Responding Trucks:  " + pRespondingUnits
-print "Job Source / Type:  " + pJobSource
-print "Address:            " + pAddress
-print "Cross Streets:      " + pCrossStreet
-print "Job Details:        " + pJobDetails
-print "Additional Info:    " + pAlarmDetails + ", " + pBoxDetails
-print "\n\nJob Number:         " + pJobNumber
+    def additional_details(self):
+        # Get optional Alarm type which is always presented with a space (Alarm
+        alarm_detials = ''.join(re.findall(r' \((Alarm .*?)\) ', sInput))
+
+        # Get optional Box type which is always presented with a space (Box
+        box_details = ''.join(re.findall(r' (\(Box .*?\))', sInput))
+
+        if alarm_detials != "" and box_details != "":
+            ad_info = alarm_detials + ", " + box_details
+        elif alarm_detials != "" and box_details == "":
+            ad_info = alarm_detials
+        elif alarm_detials == "" and box_details != "":
+            ad_info = box_details
+
+        return ad_info
